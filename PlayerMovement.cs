09c5ee65f567c40ddab7eced383fb0e1cf29dd80
Marco Octavio Luna Vargas <marco.octavio.luna@gmail.com>
2021-01-22 17:08:46 -0400

    Commit 2, en se agrego codigo para que el player sigiera al mouse, se modifico el script PlayerMovement

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -13,5 +13,16 @@ public class PlayerMovement : MonoBehaviour
     void Update(){
         //Esto es para que en la consola nos aparesca la posicion del mouse
         Debug.Log(Input.mousePosition);
+
+        //Esta en Vector 3 para que podamos acceder a los 3 ejes "x", "y" y "z"
+        //Esto es para que el player siga al mouse, es para el juego en celular y sigua nuestro dedo donde estemos 
+        //Camera.main.ScreenToWorldPoint esto es porque el mouse se mueve en pixeles y el player no, esto es para poder transformarlo
+        //Input.mousePosition esto es la posicion del mouse
+        Vector3 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //Esto es para que al mover el player con las cordenadas del mouse, pero no en z por que el player desapareceria 
+        //transform.position esta es la posicion de el player
+        //new Vector3 esto es para poder crear un nuevo vector 3, que tiene 3 cordenas("x", "y", "z")
+        transform.position = new Vector3(MousePos.x, MousePos.y, transform.position.z);
     }
 }
