db0af4a53387fa0bff633b19c6bf41bb8f1b9246
Marco Octavio Luna Vargas <marco.octavio.luna@gmail.com>
2021-01-22 19:30:44 -0400

    Commit 3, se agrego un nuevo script para el manejo de la bolita del pong, se llama BallBehaviour

---
+++ BallBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+    //VARIABLES//
+    public 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
