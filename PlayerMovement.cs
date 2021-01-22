ea8b021d1afd96a22cba7bfc461f860f3f8579b5
Marco Octavio Luna Vargas <marco.octavio.luna@gmail.com>
2021-01-22 17:50:45 -0400

    Commit 3, se acabo de codificar el script PlayerMovement, lo que se hiso es ahora solo se puede mover en el eje Y, como es un juego de Pong...

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -20,9 +20,11 @@ public class PlayerMovement : MonoBehaviour
         //Input.mousePosition esto es la posicion del mouse
         Vector3 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        //Esto es para que al mover el player con las cordenadas del mouse, pero no en z por que el player desapareceria 
+        //Esto es para que al mover el player con las cordenadas del mouse, pero no en z por que el player desapareceria, y en x no tendria sentido por que es un juego pong 
         //transform.position esta es la posicion de el player
         //new Vector3 esto es para poder crear un nuevo vector 3, que tiene 3 cordenas("x", "y", "z")
-        transform.position = new Vector3(MousePos.x, MousePos.y, transform.position.z);
+        //Mathf.Clamp(MousePos.y, -5.5f, 5.5f) esto es para restringir que el player se salga fuera de la pantalla
+        //Mathf.Clamp(VALOR A RESTRINGIR, VALOR MINIMO, VALOR MAXIMO)
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(MousePos.y, -5.5f, 5.5f), transform.position.z);
     }
 }
