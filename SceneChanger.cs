805ba4a012d164394c795764079ba55cf5ac4b23
Marco Octavio Luna Vargas <marco.octavio.luna@gmail.com>
2021-01-25 14:19:23 -0400

    Commit 8, se modifico el script de la escena para hacer las transiciones, el script se llama SceneChanger

--- SceneChanger.cs
+++ SceneChanger.cs
@@ -1,18 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;  //Esto es para poder controlar las escenas
+
 
 public class SceneChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    //Esto es para poder manejar las escenas la variable tiene que ser el nombre de la escena a la cual tiene que cambiar
+    public void ChangeSceneTo(string sceneName){
+        //Esto es para cambiar de escenas
+        SceneManager.LoadScene(sceneName);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
